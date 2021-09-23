a8a93315612eb2add6778cb541e0d256b725153f
Lautaro-L <lautilazo@gmail.com>
2021-09-23 15:10:07 -0300

    able to get time and pairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 import json
 from typing import NewType
@@ -6,30 +7,78 @@ import requests
 from datetime import date
 from datetime import datetime
 from threading import Thread
+import dateutil.parser as dparser
 
 ARTICLES_URL = 'https://www.binance.com/bapi/composite/v1/public/cms/article/catalog/list/query?catalogId=48&pageNo=1&pageSize=15'
 ARTICLE      = 'https://www.binance.com/bapi/composite/v1/public/cms/article/detail/query?articleCode='
 
-key_words = ['Binance Will List', 'adds', 'updates', 'enabled', 'trade', 'support']
+key_words = ['Futures', 'Isolated', 'Margin', 'Launchpool', 'Launchpad', 'Cross', 'Perpetual']
+filter_List = ['body', 'type', 'catalogId', 'catalogName', 'publishDate']
 
+file = 'announcements.json'
 
 def get_Announcements():
-        articles = requests.get(ARTICLES_URL).json()
-        return articles['data']['articles']
+    unfiltered_Articles = requests.get(ARTICLES_URL).json()['data']['articles']
+    articles = []
 
-def get_Pari_and_DateTime(ARTICLE_CODE):
-    new_Coin = requests.get(ARTICLE+ARTICLE_CODE).json()
-    print(new_Coin['data']['seoDesc'])
+    for article in unfiltered_Articles:
+        flag = True
+        for word in key_words:
+            if word in article['title']:
+                flag = False
+        if flag:
+            articles.append(article)
+                
+
+    for article in articles:
+        for undesired_Data in filter_List:
+            if undesired_Data in article:
+                del article[undesired_Data]
+    
+    return articles
+
+
+def get_Pair_and_DateTime(ARTICLE_CODE):
+    new_Coin = requests.get(ARTICLE+ARTICLE_CODE).json()['data']['seoDesc']
+    datetime = dparser.parse(new_Coin, fuzzy=True, ignoretz=True)
+    pair = re.findall('\S{2,6}?/USDT', new_Coin)
+    print(datetime)
+    print(pair)
+    
+
+
+
+def save_json(file, order):
+    with open(file, 'w') as f:
+        json.dump(order, f, indent=4)
+
+def load_json(file):
+    with open(file, "r+") as f:
+        return json.load(f)
+
+
+
+
+
 
 def main():
+
+    if os.path.exists(file):
+        existing_Anouncements = load_json(file)
+
+    else:
+        existing_Anouncements = get_Announcements()
+        save_json(file, existing_Anouncements)
+    
+    
+    
     new_Anouncements = get_Announcements()
 
     for announcement in new_Anouncements:
-        if key_words[0] in announcement['title']:
-            get_Pari_and_DateTime(announcement['code'])
+        if not announcement in existing_Anouncements:
+            get_Pair_and_DateTime(announcement['code'])
 
 #TODO:
-# save notice code to be able to check for new coins announcements
 # after the purchase is done save in on database
 # posible integration with AWS lambda ping it time before the coin is listed so it can place a limit order a little bti more than opening price
 # add config files
