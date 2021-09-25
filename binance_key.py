9881f429ef0ac0f52e5347784c5b5c24a514cee6
Lautaro-L <lautilazo@gmail.com>
2021-09-25 17:14:49 -0300

    added exception for auth file

--- binance_key.py
+++ binance_key.py
@@ -1,10 +1,12 @@
 import yaml
-
+import os
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
 
 
 def load_binance_creds(file):
-    with open(file) as file:
-        auth = yaml.load(file, Loader=yaml.FullLoader)
-    return Client(api_key = auth['binance_key'], api_secret = auth['binance_secret'])
+    if os.path.exists(file):
+        with open(file) as file:
+            auth = yaml.load(file, Loader=yaml.FullLoader)
+        return Client(api_key = auth['binance_key'], api_secret = auth['binance_secret'])
+    else: print("Cant find: ", file)
